b8b8de35b3bf79f32ff7eafeac765e83256168c1
Shaum <105980825+Astroneural@users.noreply.github.com>
2022-06-15 15:10:32 -0500

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -8,10 +8,10 @@ export const site: SiteConfig = {
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    name: 'John Doe',
+    name: 'Shaum',
     photo: '/assets/maskable@192.png',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'hey isn\'t this nice.'
   },
   themeColor: '#3D4451'
 }
